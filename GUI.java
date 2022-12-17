a4e2f4541b3f102efb65864abb608dcd0aaa8ea4
Marie Cari <mcari@nyc.yearup.org>
2022-12-17 13:26:05 -0500

    added button and label

--- GUI.java
+++ GUI.java
@@ -7,11 +7,17 @@ public class GUI {
     public GUI() {
         JFrame frame = new JFrame();
 
+        JButton button = new JButton("Click me!");          //create button
+        JLabel label = new JLabel("Number of clicks: 0");   //add label to button
+
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0,1));
+        panel.add(button);      //add button to panel
+        panel.add(label);       //add label to panel
 
-        frame.add(panel, BorderLayout.CENTER);
+
+        frame.add(panel, BorderLayout.CENTER);         //add panel to frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("My First GUI");
         frame.pack();
