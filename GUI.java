58e0652248caa71692fe81ca800c8ef60ffd67ef
Marie Cari <mcari@nyc.yearup.org>
2022-12-17 13:32:42 -0500

    added action listener to button to increment count when clicked

--- GUI.java
+++ GUI.java
@@ -1,14 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+   //declare counter variable
+    private int count = 0;
+    //label
+    private JLabel label;
 
     //construcor
     public GUI() {
         JFrame frame = new JFrame();
 
         JButton button = new JButton("Click me!");          //create button
-        JLabel label = new JLabel("Number of clicks: 0");   //add label to button
+        button.addActionListener(this);     //add listener to button object
+
+        label = new JLabel("Number of clicks: 0");   //add label to button //"this" attaches it to this GUI class so then add 'implements actionlistener to class
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
@@ -28,6 +36,13 @@ public class GUI {
     public static void main(String[] args) {
 
         new GUI();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
 
     }
 }
